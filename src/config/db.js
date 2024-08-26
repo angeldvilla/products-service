8694b862ad1198a31d4bb101a6c480e463fd11cb
angeldvilla <escarlata.2003@gmail.com>
2024-08-26 10:07:04 -0500

    change name db for products_db

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('catalog_db', 'root', '', {
+const sequelize = new Sequelize('products_db', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 });
