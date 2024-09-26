2def20f5b05381f115f83917ae7570edee437b7d
angeldvilla <escarlata.2003@gmail.com>
2024-09-26 16:23:26 -0500

    Add config in products server

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const productsWSDL = require('fs').readFileSync('src/wsdl/products.wsdl', 'utf8'
 const categoriesWSDL = require('fs').readFileSync('src/wsdl/categories.wsdl', 'utf8');
 
 const app = express();
+const port = 3001
 
 app.name = 'Products API';
 
@@ -32,13 +33,67 @@ app.use((req, res, next) => {
 sequelize.authenticate()
     .then(() => {
         console.log('La conexión se ha establecido correctamente.');
-        
-        const server = app.listen(3001, function () {
-            console.log('Servidor SOAP corriendo en el puerto 3001');
+
+        // rutas
+        app.get("/categories", (req, res) => {
+            res.send(`
+        <html>
+            <head>
+            <title>Servicio de Categorias</title>
+            </head>
+            <body>
+            <h1>Bienvenido al Servicio de Categorias</h1>
+            <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
+            <ul>
+                <li>Obtener todas las categorias: <code>getAllCategories</code></li>
+                <li>Crear una categoria: <code>createCategory</code></li>
+            </ul>
+            <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
+            <a href="/categories/wsdl">Ver WSDL</a>
+            </body>
+        </html>
+        `);
+        }
+        );
+
+        app.get("/products", (req, res) => {
+            res.send(`
+        <html>
+            <head>
+            <title>Servicio de Productos</title>
+            </head>
+            <body>
+            <h1>Bienvenido al Servicio de Productos</h1>
+            <p>Este servicio SOAP permite realizar las siguientes operaciones:</p>
+            <ul>
+                <li>Obtener todos los productos: <code>getAllProducts</code></li>
+                <li>Crear un producto: <code>createProduct</code></li>
+            </ul>
+            <p>Puedes ver el archivo WSDL desde el siguiente enlace:</p>
+            <a href="/products/wsdl">Ver WSDL</a>
+            </body>
+        </html>
+        `);
+        }
+        );
+
+        // Servir archivos WSDL
+        app.get('/categories/wsdl', (req, res) => {
+            res.set('Content-Type', 'text/xml');
+            res.send(categoriesWSDL);
         });
-        
-        soap.listen(server, '/products', productsService, productsWSDL);
-        soap.listen(server, '/categories', categoryService, categoriesWSDL);
+
+        app.get('/products/wsdl', (req, res) => {
+            res.set('Content-Type', 'text/xml');
+            res.send(productsWSDL);
+        });
+
+        app.listen(port, function () {
+            soap.listen(app, '/categories', categoryService, categoriesWSDL);
+            soap.listen(app, '/products', productsService, productsWSDL);
+            console.log(`Servidor SOAP corriendo en el puerto ${port}`);
+        });
+
     })
     .catch(err => {
         console.error('No se puede conectar a la base de datos:', err);
